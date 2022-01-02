ff9656fbe256a1c46d86e0b085e402e5b7efdb82
ozanarmagan <oza-armagan@yandex.com>
2022-01-02 23:19:34 +0300

    Fix

---
+++ Utils/Queue.h
@@ -0,0 +1,50 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+
+template <typename T>
+class Queue
+{
+    public:
+        Queue() { head = new T[capacity]; };
+        ~Queue() { delete [] head;}
+        void insert(const T& val);
+        T pop();
+        const size_t size() const { return total; };
+    private:
+        T* head;
+        size_t total = 0;
+        size_t capacity = 100000;
+        void increaseCapacity();
+};
+
+
+template <typename T>
+void Queue<T>::insert(const T& val)
+{
+    if(++total == capacity)
+        increaseCapacity();
+    head[total] = val;
+}
+
+template <typename T>
+T Queue<T>::pop()
+{
+    T val = head[0];
+    for(int i = 1;i < total;i++)
+        head[i - 1] = head[i];
+    total--;
+    return val;
+}
+
+
+template <typename T>
+void Queue<T>::increaseCapacity()
+{
+    auto oldHead = head;
+    capacity += 10000;
+    head = new T[capacity];
+    delete [] oldHead;
+}
+
+#endif
--- Utils/handler.h
+++ Utils/handler.h
@@ -4,12 +4,17 @@
 
 #define RUN_FOREVER while(1)
 
-#define DISPLAY_WIDTH 640
-#define DISPLAY_HEIGHT 600
+#define DISPLAY_WIDTH 1024
+#define DISPLAY_HEIGHT 960
 #define RENDER_WIDTH 256
 #define RENDER_HEIGHT 240
 #define DEBUG_WIDTH 270
 #define DEBUG_HEIGHT 270
+
+
+
+#define CYCLES_PER_SAMPLE 
+#define CYCLES_PER_BUFFER CYCLES_PER_SAMPLE * 512
 
 
 #define SET_BIT(byte,bit)   ((byte) |=  (1<<(bit)))
@@ -21,8 +26,10 @@ using std::string;
 
 typedef uint8_t BYTE;
 typedef uint16_t ADDRESS;
+typedef uint16_t AUDIO;
 typedef uint32_t PIXEL;
-typedef uint64_t TIME;
+typedef uint32_t ADDRESS32;
+typedef uint64_t TIMER;
 typedef uint8_t FLAG;
 
 struct FileHeader  // iNES File Header
